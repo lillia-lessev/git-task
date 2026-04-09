9bfdcff7c0220694291b1f16957750663d23684a
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-04-09 18:08:27 +0200

    Conversions Commit

---
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/Code Files/conversion.py
@@ -0,0 +1,25 @@
+"""
+----------------------------------------------
+
+Lillia Lessev
+    
+Variables - Storing Data in Programs
+
+----------------------------------------------
+
+"""
+# declaring variables   
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# converting variables
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+# displaying data
+print(f"{num1}\n{num2}\n{num3}\n{string1}")
+
