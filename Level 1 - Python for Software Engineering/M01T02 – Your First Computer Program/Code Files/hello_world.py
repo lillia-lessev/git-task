3f4f8d5ac51cdd6b3e0900e03c0a8edad32b7850
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-04-09 16:50:46 +0200

    Hello World Commit

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/Code Files/hello_world.py
@@ -0,0 +1,31 @@
+"""
+---------------------------------
+Lillia Lessev
+
+My First Computer Program
+---------------------------------
+   
+"""
+
+
+
+"""
+Pseudocode
+    
+--------------
+insert user name
+insert user age
+display user name
+display user age
+display "Hello World!"
+    
+
+"""
+
+insert_user_name = input("Please enter your name: ")
+insert_user_age = input("Please enter your age: ")
+print("Your name is: " + insert_user_name + "\nYour age is: " + insert_user_age + "\nHello World!")
+
+
+
+
