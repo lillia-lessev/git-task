83433825457543386e3698b061cb1a40cac50173
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-04-15 21:16:27 +0200

    Pattern Commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/Code Files/pattern.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/Code Files/pattern.py
@@ -8,3 +8,26 @@ Iteration and loops
 ----------------------------------------------
 
 """
+
+num = 0
+stars = ""
+string_length = 0
+
+print("\n\n")
+
+for i in range(1, 9):
+    if (i < 6):
+        stars = stars + "*"
+        string_length = len(stars)
+        print(stars)
+    elif (i >=6):
+        string_length = len(stars)
+        empty_char = ""
+        stars = stars[:(string_length - 1)]
+        string_length = len(stars)
+        print(stars)
+    
+    
+print("\n\n")
+
+    
