187c1731d6f7d4274a878f3864f6e6f02976366f
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-04-15 21:55:02 +0200

    Logic Commit

---
+++ Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/logic.py
@@ -0,0 +1,16 @@
+"""
+----------------------------------------------
+
+Lillia Lessev
+
+Defensive Programming
+----------------------------------------------
+
+"""
+
+# THIS PROGRAM SHOWS LOGICAL ERRORS
+
+num1 = 10
+num2 = 20
+
+print(str(num1) + " + " + str(num2) + " = " + str(num1) + str(num2))
