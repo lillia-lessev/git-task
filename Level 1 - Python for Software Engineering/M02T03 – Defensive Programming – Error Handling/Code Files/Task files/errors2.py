8d04bcb3097536e20a21242f558b82abe3b7d508
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-04-15 21:49:00 +0200

    Errors 2 Commit

--- Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
+++ Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
@@ -2,6 +2,9 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
+''' 
+Original code:
+
 animal = Lion
 animal_type = "cub"
 number_of_teeth = 16
@@ -9,4 +12,16 @@ number_of_teeth = 16
 full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
 
 print full_spec
+
+
+'''
+
+
+animal = "Lion" # added quation marks
+animal_type = "cub"
+number_of_teeth = 16
+
+full_spec = (f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth.") # added brackets () and braces {} for correct f-string format. Fixed order of variables in sentence, spacing etc.
+
+print(full_spec) # added brackets for print statement
 
