eb2e0226cd4bd99c4e74264643b2c69e1ca54c8c
Lillia Lessev <lillia.lessev.scholar@gmail.com>
2026-05-13 19:52:41 +0200

    Sum Recursion Commit

--- Level 1 - Python for Software Engineering/M03T05 – Recursion/Code files/sum_recursion.py
+++ Level 1 - Python for Software Engineering/M03T05 – Recursion/Code files/sum_recursion.py
@@ -11,22 +11,20 @@ Recursion
 
 print("\n--------------------------------------------------------\n")
 
+
 def sum(num_list, index):
-    # Sum all nums in list from position 0 until and including specified index   
+    # Sum all nums in list from position 0 until and including specified index
     if index <= 0:
-        result = num_list[index] 
+        result = num_list[index]
         return result
     else:
         result = num_list[index] + sum(num_list, (index-1))
         return result
-    
 
 
 num_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
 for index in range(len(num_list)):
     print(f"Result: {sum(num_list, index)}")
-
-
 
 print("\n--------------------------------------------------------\n")
